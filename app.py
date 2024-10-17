e6958745b67959d5c73208f37bda83ecdaab8c9f
Hritik kaliyar <hritikkaliyar@gmail.com>
2024-10-17 15:54:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+nltk.download('punkt')
+nltk.download('stopwords')
 lemmatizer=WordNetLemmatizer()
 app1=Flask(__name__,template_folder='template')
 app=app1
